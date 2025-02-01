0d89103ad30f7db86c28ae35b466a6a1c6552149
Sanford Staab <sanfords@gmail.com>
2025-02-01 12:41:45 -0800

    update syncAWS.cmd to create churchSongs.zip and propagate to S3

--- syncAWS.cmd
+++ syncAWS.cmd
@@ -1,3 +1,5 @@
-aws s3 sync . s3://sandystransfer/churchSongs --exclude .gitignore --exclude "Music Sheets" --exclude .git/* --exclude syncAWS.cmd --acl public-read
+7z a churchSongs.zip Src Data
+aws s3 sync . s3://sandystransfer/churchSongs --exclude * --include churchSongs.zip --acl public-read
+@REM aws s3 sync . s3://sandystransfer/churchSongs --exclude .gitignore --exclude "Music Sheets" --exclude .git/* --exclude syncAWS.cmd --acl public-read
 @echo run with https://sandystransfer.s3.us-west-2.amazonaws.com/churchSongs/src/churchSongs.html
 @echo data file: "https://sandystransfer.s3.us-west-2.amazonaws.com/churchSongs/Data/Stites Baptist Church Song Library.json"
